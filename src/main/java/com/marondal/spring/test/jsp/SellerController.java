b328212980b003cd164eb974ddf265e011748f08
megaIt202 <megait202@gmailcom>
2024-02-26 18:20:27 +0900

    [김인규]  - jsp 판매자  문제 풀이

---
+++ src/main/java/com/marondal/spring/test/jsp/SellerController.java
@@ -0,0 +1,61 @@
+package com.marondal.spring.test.jsp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.marondal.spring.test.jsp.domain.Seller;
+import com.marondal.spring.test.jsp.service.SellerService;
+
+@RequestMapping("/jsp/seller")
+@Controller
+public class SellerController {
+	
+	@Autowired
+	private SellerService sellerService;
+	
+	@PostMapping("/create")
+	@ResponseBody
+	public String createSeller(
+			@RequestParam("nickname") String nickname
+			, @RequestParam("temperature") double temperature
+			, @RequestParam("profileImage") String profileImage) {
+		
+		int count = sellerService.addSeller(nickname, temperature, profileImage);
+		
+		return "수행 결과 : " + count;
+		
+	}
+	
+	@GetMapping("/input")
+	public String inputSeller() {
+		return "jsp/sellerInput";
+	}
+	
+	@GetMapping("/info")
+	public String sellerInfo(
+			@RequestParam(value="id", required=false) Integer id
+			, Model model) {
+				
+		Seller seller = null;
+		// id 파라미터가 전달되면 id에 일치하는 판매자 정보 조회
+		if(id != null) {			
+			seller = sellerService.getSeller(id);
+			
+		} else {  // id 파라미터가 전달되지 않으면 가장 최근에 추가된 판매자 정보 조회 		 	
+			seller = sellerService.getLastSeller();
+			
+		}
+		
+		model.addAttribute("seller", seller);
+		return "jsp/sellerInfo";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/marondal/spring/test/jsp/repository/SellerRepository.java
@@ -0,0 +1,21 @@
+package com.marondal.spring.test.jsp.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.marondal.spring.test.jsp.domain.Seller;
+
+@Mapper
+public interface SellerRepository {
+	
+	public int insertSeller(
+			@Param("nickname") String nickname
+			, @Param("temperature") double temperature
+			, @Param("profileImage") String profileImage);
+
+	public Seller selectLastSeller();
+	
+	public Seller selectSeller(@Param("id") int id);
+	
+	
+}
